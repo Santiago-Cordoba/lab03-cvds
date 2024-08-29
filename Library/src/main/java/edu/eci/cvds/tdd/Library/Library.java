a8cd4e68863bf6af0296a6f8afc9f50eb863112c
santiago coronado <156589422+Santiago12323@users.noreply.github.com>
2024-08-29 18:33:09 -0500

    creacion del metodo addbook despues del test

--- Library/src/main/java/edu/eci/cvds/tdd/Library/Library.java
+++ Library/src/main/java/edu/eci/cvds/tdd/Library/Library.java
@@ -37,8 +37,8 @@ public class Library {
      * @return true if the book was stored false otherwise.
      */
     public boolean addBook(Book book) {
-        //TODO Implement the logic to add a new book into the map.
-        return false;
+        books.put(book,1);
+        return true;
     }
 
 
