ef21f38473cb2c02eca0e186878c015ec487e506
santiago Coronado <santiago.coronado-p@labinfo.is.escuelaing.edu.co>
2024-08-29 15:55:05 -0500

    primera prueba addBock

---
+++ Library/src/test/java/edu/eci/cvds/tdd/Library/LibraryTest.java
@@ -0,0 +1,39 @@
+package edu.eci.cvds.tdd.library;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.loan.Loan;
+import edu.eci.cvds.tdd.library.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+        book = new Book("10002", "Homero", "Opera"); // Corrige el orden de los parámetros si es necesario
+    }
+
+    @Test
+    public void testAddBook() {
+        library.addBook(book);
+        Map<Book, Integer> books = library.getBooks();
+        boolean respuesta = books.containsKey(book);
+        assertTrue(respuesta, "The book should be in the library");
+    }
+
+    
+}
